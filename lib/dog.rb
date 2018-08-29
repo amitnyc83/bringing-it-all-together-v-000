5827c3279b7963e7fde740602edd163c22a356ff
Amit Singh <amit_nyc@live.com>
2018-08-29 18:15:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -122,7 +122,7 @@ def self.new_from_db(row)
  id = row[0]
  name = row[1]
  breed = row[2]
- new_dog = self.new(id: id, name: name, breed: breed)
+ new_dog = self.new(id, name, breed)
  new_dog
 end
 
